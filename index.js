afa940ece131b4744b6a11c7240ac75356283d20
Victoria Miranda <vitorianevesmiranda@gmail.com>
2021-11-04 22:53:23 -0300

    ultimas atualizações

--- index.js
+++ index.js
@@ -1,23 +1,71 @@
 const cards = document.querySelectorAll('.card');
-let hasFlippedCard = false;
+let flippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
-//virar
+//card
 function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
 
     this.classList.add('flip');
 
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
+    if (!flippedCard) {
+        flippedCard = true;
         firstCard = this;
         return;
     }
 
     secondCard = this;
-    hasFlippedCard = false;
+    flippedCard = false;
 
     checkForMatch();
 }
+
+//match
+function checkForMatch() {
+    if (firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+//disable
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+//flip
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1500);
+}
+//reset
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//shuffle
+(function shuffle() {
+    cards.forEach((card) => {
+        let randomPosition = Math.floor(Math.random() * 12);
+        card.style.order = randomPosition;
+    })
+})();
+
+//flipCard
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard);
+});
